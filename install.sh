e226cc07b89bf003dcc616efbf6d47a530938cd7
William Gerhard <willig42@vt.edu>
2018-04-12 20:56:54 -0400

    fixed error.

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ opkg install python-codecs
 opkg install python-logging
 opkg install python-openssl
 opkg install pyOnionGpio
-wget --no-check-certificate https://github.com/williamg42/amp-lab-http-rover/archive/master.tar.gz
-tar -xvzf master.tar.gz
+
 cd /amp-lab-http-rover
 wget --no-check-certificate https://github.com/defnull/bottle/raw/master/bottle.py
 
