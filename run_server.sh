07dcba9d60b7f49ab68e175ee7ee8e1cb309e72e
williamg42 <willig2@vt.edu>
2018-09-10 16:49:27 -0400

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -3,6 +3,6 @@
 cd /root/amp-lab-http-rover-master/
 gpioctl dirout-low 19
 gpioctl dirout-low 18
-gpioctl dirout-low 17
-gpioctl dirout-low 11
-python ./main.py 19 17
+gpioctl dirout-low 16
+gpioctl dirout-low 15
+python ./main.py 19 16 18 15 
