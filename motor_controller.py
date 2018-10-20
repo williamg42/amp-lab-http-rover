349df0f08a762a7804642615f627a2195d7c3046
William Gerhard <willig42@vt.edu>
2018-10-20 16:25:11 -0400

    reduced os.system calls by 50%

--- motor_controller.py
+++ motor_controller.py
@@ -28,29 +28,37 @@ class MotorController:
 
 
 
-        if R < 0: #if robot is going forward
+        if R < 0 && L < 0: #if robot is going forward
 
             
             command = "fast-gpio pwm %d %d %d && " % (self.right_motor_f, 1000, abs(R))
-            command = command + "fast-gpio pwm %d %d %d" % (self.right_motor_r, 1000, 0)
+            command = command + "fast-gpio pwm %d %d %d &&" % (self.right_motor_r, 1000, 0)
+            command = command + "fast-gpio pwm %d %d %d && " % (self.left_motor_f, 1000, abs(L))
+            command = command + "fast-gpio pwm %d %d %d" % (self.left_motor_r, 1000, 0)
             os.system(command)
-            print(command)
+           
 
 
-        else: 
+        elif R >= 0 && L < 0: 
 
             command = "fast-gpio pwm %d %d %d && " % (self.right_motor_f, 1000, 0)
-            command = command + "fast-gpio pwm %d %d %d" % (self.right_motor_r, 1000, R)
+            command = command + "fast-gpio pwm %d %d %d && " % (self.right_motor_r, 1000, R)
+            command = command + "fast-gpio pwm %d %d %d && " % (self.left_motor_f, 1000, abs(L))
+            command = command + "fast-gpio pwm %d %d %d" % (self.left_motor_r, 1000, 0)
             os.system(command) 
 
 
-        if L < 0: #if robot is going forward
-            command = "fast-gpio pwm %d %d %d && " % (self.left_motor_f, 1000, abs(L))
-            command = command + "fast-gpio pwm %d %d %d" % (self.left_motor_r, 1000, 0)
+        elif R < 0 && L >= 0: #if robot is going forward
+            command = "fast-gpio pwm %d %d %d && " % (self.right_motor_f, 1000, abs(R))
+            command = command + "fast-gpio pwm %d %d %d &&" % (self.right_motor_r, 1000, 0)
+             ommand = command + "fast-gpio pwm %d %d %d && " % (self.left_motor_f, 1000, 0)
+            command = command + "fast-gpio pwm %d %d %d" % (self.left_motor_r, 1000, L)           
             os.system(command)          
 
         else: 
 
-            command = "fast-gpio pwm %d %d %d && " % (self.left_motor_f, 1000, 0)
-            command = command + "fast-gpio pwm %d %d %d" % (self.left_motor_r, 1000, L)
-	    os.system(command) 
+            command = "fast-gpio pwm %d %d %d && " % (self.right_motor_f, 1000, 0)
+            command = command + "fast-gpio pwm %d %d %d &&" % (self.right_motor_r, 1000, R)
+             ommand = command + "fast-gpio pwm %d %d %d && " % (self.left_motor_f, 1000, 0)
+            command = command + "fast-gpio pwm %d %d %d" % (self.left_motor_r, 1000, L)           
+            os.system(command) 
