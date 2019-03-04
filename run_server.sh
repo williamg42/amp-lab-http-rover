c4a2091f5c387afe43799e8e8d5509060055b2dd
williamg42 <willig2@vt.edu>
2019-03-04 17:53:39 -0500

    Update run_server.sh
    Hardware PWM to reduce CPU overhead?

--- run_server.sh
+++ run_server.sh
@@ -5,11 +5,15 @@ gpioctl dirout-low 19
 gpioctl dirout-low 18
 gpioctl dirout-low 16
 gpioctl dirout-low 15
-fast-gpio set-output 19
-fast-gpio set-output 18
+
 fast-gpio set-output 16
 fast-gpio set-output 15
 
+omega2-ctrl gpiomux set pwm0 pwm
+omega2-ctrl gpiomux set pwm1 pwm
+
+onion pwm 0 0 50
+onion pwm 1 0 50
 
 iptables -t nat -I PREROUTING --src 0/0 --dst 192.168.3.1 -p tcp --dport 80 -j REDIRECT --to-ports 8000
 
