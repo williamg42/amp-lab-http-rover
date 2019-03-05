11c2ee731a1dd55283c4b0f3b34fb80f673f0a1c
williamg42 <willig2@vt.edu>
2019-03-05 11:49:12 -0500

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -9,12 +9,6 @@ gpioctl dirout-low 15
 fast-gpio set-output 16
 fast-gpio set-output 15
 
-omega2-ctrl gpiomux set pwm0 pwm
-omega2-ctrl gpiomux set pwm1 pwm
-
-onion pwm 0 0 50
-onion pwm 1 0 50
-
 iptables -t nat -I PREROUTING --src 0/0 --dst 192.168.3.1 -p tcp --dport 80 -j REDIRECT --to-ports 8000
 
 
